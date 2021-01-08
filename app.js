98742a57a4fa30dffc1f9d75e5ffd8ccc1cdb889
ADVAITH U <45172876+advaith-unnikrishnan@users.noreply.github.com>
2021-01-08 19:31:33 +0530

    testing :  testing with a mock API

--- app.js
+++ app.js
@@ -2,6 +2,32 @@ var input = document.querySelector("#input")
 var btn = document.querySelector("#translate-btn")
 var output = document.querySelector("#output")
 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function getTranslateUrl(text){
+    return serverURL+"?text="+text
+}
+
+function errorHandler(error){
+    console.log(" Error occured : ",error)
+    alert("Oops! something wrong with the server, Try again after sometime")
+}
+
 btn.addEventListener("click" , ()=>{
-    output.innerText = input.value 
+    var text = input.value 
+    var url = getTranslateUrl(text)
+    fetch(url)
+    .then(response => response.json())  //converting response to json format
+    .then(json => {
+        output.innerText = json.contents.translated
+    })
 })
+
+
+
+
+
+
+
+
+// https://api.funtranslations.com/translate/valyrian.json?text=Fire is blood
